bfecfbb36fce5ba5d484fba0d273bd5cb035df7c
Matt Boggs <matt.david.boggs@gmail.com>
2020-10-19 18:24:29 -0600

    cleaning and tidying

--- app.js
+++ app.js
@@ -10,18 +10,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 let empArr = []
-
 beginAsking = () => {
-    inquirer.prompt(
-        {
+    inquirer.prompt({
             type: "list",
             message: "What type of employee are you adding?",
             choices: [Manager,Engineer,Intern,"No more employees"],
             name: "empType"
-        }
-    ).then(function(response){
-    console.log(response.empType)
-
+    }).then(function(response){
+        
         switch(response.empType){
             case "Manager":
                 let questionsM = [{
@@ -47,7 +43,6 @@ beginAsking = () => {
                 inquirer.prompt(questionsM).then(function(response){
                     let newEmployee = new Manager(response.name,response.id,response.email,response.officeNumber)
                     empArr.push(newEmployee)
-                    console.log(empArr)
                     beginAsking()
                 })
                 break;
@@ -75,7 +70,6 @@ beginAsking = () => {
                 inquirer.prompt(questionsE).then(function(response){
                     let newEmployee = new Engineer(response.name,response.id,response.email,response.github)
                     empArr.push(newEmployee)
-                    console.log(empArr)
                     beginAsking()
                 })
                 break;
@@ -103,7 +97,6 @@ beginAsking = () => {
                 inquirer.prompt(questionsI).then(function(response){
                     let newEmployee = new Intern(response.name,response.id,response.email,response.school)
                     empArr.push(newEmployee)
-                    console.log(empArr)
                     beginAsking()
                 })
                 break;
@@ -114,32 +107,6 @@ beginAsking = () => {
                     }
                 })
         }
-        console.log(empArr)
     })
 }
 beginAsking()
-// TEMPLATE LITERAL COMING UP
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-// fs.writeFile("./output/team.html", output)
